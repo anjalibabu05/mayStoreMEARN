67c21ad06d85581739625a0407fbc4fc453b0d7b
Anjali Babu <147030995+anjalibabu05@users.noreply.github.com>
2026-06-12 08:28:00 +0530

    Refactor API functions for cleaner syntax

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,133 +1,66 @@
 import { commonApi } from "./commonApi";
 import { serverUrl } from "./serverUrl";
 
-// register POST, reqbody ,content tyoe application/json
-export const registerApi= async (reqBody)=>{
-    return await commonApi('POST',`${serverUrl}/register`,reqBody)
-}
+// AUTH
+export const registerApi = async (reqBody) =>
+  await commonApi('POST', `${serverUrl}/register`, reqBody);
 
-// login post,reqbody
-export const loginApi= async (reqBody)=>{
-    return await commonApi('POST',`${serverUrl}/login`,reqBody)
-}
+export const loginApi = async (reqBody) =>
+  await commonApi('POST', `${serverUrl}/login`, reqBody);
 
-// Google login POST,CREDENTIALS,REQBODY
-export const googleLoginApi = async (reqBody)=>{
-    return await commonApi("POST",`${serverUrl}/google-login`,reqBody)
-}
+export const googleLoginApi = async (reqBody) =>
+  await commonApi("POST", `${serverUrl}/google-login`, reqBody);
 
-// get home book
-export const homeBookApi=async()=>{
-    return await commonApi("GET",`${serverUrl}/all-home-books`,'')
-}
+// PUBLIC
+export const homeBookApi = async () =>
+  await commonApi("GET", `${serverUrl}/all-home-books`, '');
 
-// get all user brought books
-// export const getAllUserBooksApi=async(reqHeader)=>{
-//     return await commonApi("GET",`${serverUrl}/all-home-books`,'')
-// }
+// USER
+export const uploadBookApi = async (reqBody, reqHeader) =>
+  await commonApi("POST", `${serverUrl}/add-book`, reqBody, reqHeader);
 
+export const getAllBookApi = async (searchKey, reqHeader) =>
+  await commonApi("GET", `${serverUrl}/all-books?search=${searchKey}`, '', reqHeader);
 
+export const getViewBookApi = async (id) =>
+  await commonApi("GET", `${serverUrl}/view-book/${id}`, '');
 
+export const addApplicationApi = async (reqBody, reqHeader) =>
+  await commonApi("POST", `${serverUrl}/apply-job`, reqBody, reqHeader);
 
-// ------------------------------------USER----------------------------------
-// upload a book
-export const uploadBookApi=async (reqBody,reqHeader)=>{
-    return await commonApi("POST", `${serverUrl}/add-book`,reqBody,reqHeader)
-}
+export const getAllUserBooksApi = async (reqHeader) =>
+  await commonApi("GET", `${serverUrl}/user-tab-books`, "", reqHeader);
 
-// get all books
-export const getAllBookApi=async(searchKey,reqHeader)=>{
+export const getAllUserBroughtBooksApi = async (reqHeader) =>
+  await commonApi("GET", `${serverUrl}/user-brought-books`, "", reqHeader);
 
-    // Query-baseurl?key=value
-    return await commonApi("GET",`${serverUrl}/all-books?search=${searchKey}`,'',reqHeader)
-}
+export const updateUserProfileApi = async (reqBody, reqHeader) =>
+  await commonApi('PATCH', `${serverUrl}/user-edit-profile`, reqBody, reqHeader);
 
+export const makepaymentApi = async (reqBody, reqHeader) =>
+  await commonApi("PUT", `${serverUrl}/make-payment`, reqBody, reqHeader);
 
-// get a View books
-export const getViewBookApi=async(id)=>{
-    return await commonApi("GET",`${serverUrl}/view-book/${id}`,'')
-}
+// ADMIN
+export const getAllAdminBookApi = async (reqHeader) =>
+  await commonApi("GET", `${serverUrl}/admin-all-books`, '', reqHeader);
 
-// add job application 
-export const addApplicationApi=async (reqBody,reqHeader)=>{
-    return await commonApi("POST",`${serverUrl}/apply-job`,reqBody,reqHeader)
-}
+export const approveBookApi = async (reqBody, reqHeader) =>
+  await commonApi("PUT", `${serverUrl}/approve-books`, reqBody, reqHeader);
 
-// export const addApplicationApi= aync (reqBody,reqH)
+export const getAllUserApi = async (reqHeader) =>
+  await commonApi("GET", `${serverUrl}/all-users`, '', reqHeader);
 
-// get all user books
-export const getAllUserBooksApi=async(reqHeader)=>{
-    return await commonApi("GET",`${serverUrl}/user-tab-books`,"",reqHeader)
-}
+export const addJobApi = async (reqBody) =>
+  await commonApi("POST", `${serverUrl}/add-job`, reqBody);
 
+export const getallJobsApi = async (searchKey) =>
+  await commonApi("GET", `${serverUrl}/all-jobs?search=${searchKey}`);
 
-// get all user brought books
-export const getAllUserBroughtBooksApi=async(reqHeader)=>{
-    return await commonApi("GET",`${serverUrl}/user-brought-books`,"",reqHeader)
-}
+export const updateProfileApi = async (reqBody, reqHeader) =>
+  await commonApi("PUT", `${serverUrl}/admin-profile-update`, reqBody, reqHeader);
 
-// --------------------------Admin---------------------------------
+export const deleteAjobApi = async (id) =>
+  await commonApi("DELETE", `${serverUrl}/delete-job/${id}`);
 
-
-export const getAllAdminBookApi=async(reqHeader)=>{
-    return await commonApi("GET",`${serverUrl}/admin-all-books`,'',reqHeader)
-}
-
-// Approve Books
-
-
-export const approveBookApi=async(reqBody,reqHeader)=>{
-    return await commonApi("PUT",`${serverUrl}/aprove-books`,reqBody,reqHeader)
-}
-
-// get All Users
-export const getAllUserApi=async (reqHeader)=>{
-    return await commonApi("GET",`${serverUrl}/all-users`,'',reqHeader)
-}
-
-// api to add a job
-export const addJobApi=async (reqBody)=>{
-    return await commonApi("POST",`${serverUrl}/add-job`,reqBody)
-}
-
-// api to get all  jobs
-export const getallJobsApi=async (searchKey)=>{
-    return await commonApi("GET",`${serverUrl}/all-jobs?search=${searchKey}`)
-}
-// update AdminInfo
-
-export const updateProfileApi=async (reqBody,reqHeader)=>{
-    return await commonApi("PUT",`${serverUrl}/admin-profile-update`,reqBody,reqHeader)
-
-}
-// update User Profile
-export const updateUserProfileApi = async (reqBody, reqHeader) => {
-  return await commonApi(
-    'PATCH',
-    `${serverUrl}/user-edit-profile`,
-    reqBody,
-    reqHeader
-  );
-};
-
-
-// get all books admin
-// export const gwtAllJobsApi=async (searchKey)=>{
-//     return await commonApi("GET",`${serverUrl}/add-job?search=${searchKey}`,reqBody)
-// }
-
-// delete a job
- export const deleteAjobApi=async(id)=>{
-     return await commonApi("GET",`${serverUrl}/delete-job/${id}`)
- }
-
-//  get all applications
-export const getAllApplicationApi=async()=>{
-    return await commonApi("GET",`${serverUrl}/all-application`)
-}
-
-// api to make payment
-export const makepaymentApi=async(reqBody,reqHeader)=>{
-    return await commonApi("PUT",`${serverUrl}/make-payment`,reqBody,reqHeader)
-}
-
+export const getAllApplicationApi = async () =>
+  await commonApi("GET", `${serverUrl}/all-application`);
